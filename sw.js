94063d3592c4b9a833e45f9a74442de67ab6e155
Haruna Lawali <lawaliharuna943@gmail.com>
2026-08-03 14:39:00 +0100

    Update service worker for cache and notification URLs

--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
-const CACHE_NAME = 'amandata-cache-v1';
+const CACHE_NAME = 'amandata-cache-v2';
 const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './register.html'
+  '/',
+  '/index',
+  '/register'
 ];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(ASSETS_TO_CACHE);
+      return cache.addAll(ASSETS_TO_CACHE).catch(function() {
+        return Promise.resolve();
+      });
     }).then(() => self.skipWaiting())
   );
 });
@@ -31,12 +33,11 @@ self.addEventListener('fetch', function(event) {
   if (event.request.mode === 'navigate') {
     event.respondWith(
       fetch(event.request).catch(function() {
-        return caches.match('./register.html');
+        return caches.match('/register') || caches.match('/');
       })
     );
     return;
   }
-
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
@@ -55,7 +56,7 @@ self.addEventListener('push', function(event) {
     badge: 'https://i.imgur.com/SBHTDNn.png',
     vibrate: [100, 50, 100],
     data: {
-      url: './register.html'
+      url: 'https://www.amandata.com.ng/register'
     }
   };
   event.waitUntil(
@@ -65,9 +66,9 @@ self.addEventListener('push', function(event) {
 
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
-  let targetUrl = new URL('./register.html', self.location.origin).href;
+  let targetUrl = 'https://www.amandata.com.ng/register';
   if (event.notification.data && event.notification.data.url) {
-    targetUrl = new URL(event.notification.data.url, self.location.origin).href;
+    targetUrl = event.notification.data.url;
   }
   event.waitUntil(
     clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
